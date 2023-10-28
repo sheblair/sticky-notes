cd98efa2cb69a654053fcac9ebc21cb6b52a9307
sheblair <66489366+sheblair@users.noreply.github.com>
2023-10-27 22:59:39 -0400

    background colors stored in browser

--- src/App.jsx
+++ src/App.jsx
@@ -3,101 +3,142 @@ import Header from "./components/Header";
 import NotesList from "./components/NotesList";
 
 export default function App() {
+  // initialize notes from browser storage or empty array
   const [notes, setNotes] = useState(
     () => JSON.parse(localStorage.getItem("notes")) || []
   );
+
+  // initialize theme from browser storage or default
   const [theme, setTheme] = useState(
-    JSON.parse(localStorage.getItem("theme")) || ""
+    JSON.parse(localStorage.getItem("theme")) || "default"
   );
 
+  // initialize theme from browser storage or default
   const [selectedThemeOption, setSelectedThemeOption] = useState(
     JSON.parse(localStorage.getItem("theme")) || "default"
   );
 
+  // initialize backgroundColors as empty array
   const [backgroundColors, setBackgroundColors] = useState([]);
+
+  // initialize themeChanged as false
   const [themeChanged, setThemeChanged] = useState(false);
+
+  // initialize searchText as empty string
   const [searchText, setSearchText] = useState("");
 
+  // define array of objects to determine theme options
+  // for reasons unknown to me, the default theme has to have a rgb value
+  // it doesn't get converted like the other values do. this is a mystery i have yet to solve
   const themeOptions = [
     {
       id: "default",
       name: "Default",
-      hexValue: "ffffe0",
+      value: "rgb(255, 255, 224)",
     },
     {
       id: "pink",
       name: "Angel",
-      hexValue: "ffe6e9",
+      value: "ffe6e9",
     },
     {
       id: "green",
       name: "Garden",
-      hexValue: "fffacd",
+      value: "fffacd",
     },
     {
       id: "lavender",
       name: "Stardust",
-      hexValue: "fde6ff",
+      value: "fde6ff",
     },
   ];
 
+  // gets called when user selects from theme dropdown menu
+  function switchTheme(selectedValue) {
+    // if the selection is different from the current theme, flip themeChanged to true
+    if (theme !== selectedValue) {
+      setThemeChanged(true);
+
+      // if the user selected the default value, set theme, selectedThemeOption to default and store in browser
+      if (selectedValue === "rgb(255, 255, 224)") {
+        setTheme("default");
+        setSelectedThemeOption("default");
+        localStorage.setItem("theme", JSON.stringify("default"));
+        // if the user selected anything other than default, set theme, selectedThemeOption and store in browser
+      } else {
+        setTheme(selectedValue);
+        setSelectedThemeOption(selectedValue);
+        localStorage.setItem("theme", JSON.stringify(selectedValue));
+      }
+    }
+  }
+
+  // effect that runs anytime theme, themeChanged are updated
   useEffect(() => {
-    if (selectedThemeOption === "default") {
-      setNotes((prevNotes) =>
-        prevNotes.map((note) => ({
-          ...note,
-          backgroundColor: "ffffe0",
-        }))
-      );
-    } else if (theme) {
+    // if the theme is default, don't do anything here
+    if (theme === "default") {
+      return;
+      // if the theme is anything other than default, fetch colors from API according to which theme is active
+    } else {
       fetch(`https://www.thecolorapi.com/scheme?hex=${theme}&mode=analogic`)
         .then((response) => response.json())
         .then((data) => {
+          /*
+           * something i don't understand - i am pulling hex values here but
+           * later somehow they will be converted to rgb when they get
+           * inserted into the HTML structure - i need to figure out why/how
+           * */
           const hexValues = data.colors.map((color) => color.hex.value);
+          // set the background colors array to be the hex values we fetched
           setBackgroundColors(hexValues);
         })
-        .catch((error) =>
-          console.log("Error fetching hex values from API", error)
-        );
+        .catch((error) => {
+          /* if the API fails, throw an error, set all note background color to default,
+          set selected theme option to default, and alert the user that feature is broken */
+          console.log("Error fetching hex values from API", error);
+          setNotes((prevNotes) =>
+            prevNotes.map((note) => ({
+              ...note,
+              backgroundColor: "rgb(255, 255, 224)",
+            }))
+          );
+          setSelectedThemeOption("default");
+          alert("Oops! Looks like the theme-switcher is broken.");
+        });
     }
   }, [theme, themeChanged]);
 
-  // when user selects a theme option, set theme to that hex value
-  function switchTheme(selectedHexVal) {
-    if (theme !== selectedHexVal) {
-      setThemeChanged(true);
-    } else if (selectedHexVal === "default") {
-      setThemeChanged(false);
-    }
-    setTheme(selectedHexVal);
-    setSelectedThemeOption(selectedHexVal); // Update the selected theme option
-    localStorage.setItem("theme", JSON.stringify(selectedHexVal));
-
-    console.log(theme);
-    console.log(selectedHexVal);
-    console.log("switchTheme ran");
-  }
-
-  // if we have hex values in the backgroundColors array, map over notes and change colors
+  // effect that runs anytime theme, themeChanged, or backgroundColors are updated
   useEffect(() => {
-    if (themeChanged && backgroundColors.length) {
+    // if we have a theme change, time to map over the notes and update the colors to reflect the new theme
+    if (themeChanged) {
       setNotes((prevNotes) =>
         prevNotes.map((note) => ({
           ...note,
           backgroundColor:
-            backgroundColors[
-              Math.floor(Math.random() * backgroundColors.length)
-            ],
+            /* if the theme is default, they should all be the same color (must be rgb val not hex val)
+            if not default, they should be random from the array of hex values we fetched */
+            theme === "default"
+              ? "rgb(255, 255, 224)"
+              : backgroundColors[
+                  Math.floor(Math.random() * backgroundColors.length)
+                ],
         }))
       );
     }
-  }, [backgroundColors, themeChanged, theme]);
+  }, [theme, themeChanged, backgroundColors]);
 
-  // save notes to local storage in the browser
+  // effect that runs every time notes is updated; stores notes in local browser
   useEffect(() => {
     localStorage.setItem("notes", JSON.stringify(notes));
   }, [notes]);
 
+  /*
+   * add a new note - if the theme is default, the color will be default,
+   * if not it will be a random color from our background colors array.
+   * if the color is default, it has to be set as a rgb value, and not a hex value.
+   * i have no idea why this is happening. it shouldn't be
+   */
   function addNote() {
     const randomColor =
       backgroundColors[Math.floor(Math.random() * backgroundColors.length)];
@@ -107,15 +148,17 @@ export default function App() {
       title: "",
       body: "",
       doesMatchSearch: true,
-      backgroundColor: theme ? randomColor : "ffffe0",
+      backgroundColor: theme === "default" ? "rgb(255, 255, 224)" : randomColor,
     };
     setNotes((prevNotes) => [newNote, ...prevNotes]);
   }
 
+  // remove a note
   function removeNote(clickedId) {
     setNotes((oldNotes) => oldNotes.filter((note) => note.id !== clickedId));
   }
 
+  // update a note's title or body text
   function onType(changedNoteId, updatedKey, updatedValue) {
     setNotes((prevNotes) =>
       prevNotes.map((note) => {
@@ -132,6 +175,7 @@ export default function App() {
     );
   }
 
+  // search through the active notes
   function onSearch(text) {
     const newSearchText = text.toLowerCase();
 
@@ -157,6 +201,7 @@ export default function App() {
   return (
     <div className="App">
       <Header
+        theme={theme}
         addNote={addNote}
         onSearch={onSearch}
         searchText={searchText}
--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Option({ option }) {
-  return <option value={option.hexValue}>{option.name}</option>;
+  return <option value={option.value}>{option.name}</option>;
 }
