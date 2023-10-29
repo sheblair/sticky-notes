c2cbce5f4770a28dea9071e1e1af483f104a764c
sheblair <66489366+sheblair@users.noreply.github.com>
2023-10-29 18:07:16 -0400

    clean up comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,28 @@ import Header from "./components/Header";
 import NotesList from "./components/NotesList";
 
 export default function App() {
-  // initialize notes from browser storage or empty array
   const [notes, setNotes] = useState(
     () => JSON.parse(localStorage.getItem("notes")) || []
   );
 
-  // initialize theme from browser storage or default
   const [theme, setTheme] = useState(
     JSON.parse(localStorage.getItem("theme")) || "default"
   );
 
-  // initialize theme from browser storage or default
   const [selectedThemeOption, setSelectedThemeOption] = useState(
     JSON.parse(localStorage.getItem("theme")) || "default"
   );
 
-  // initialize backgroundColors as empty array
   const [backgroundColors, setBackgroundColors] = useState([]);
-
-  // initialize themeChanged as false
   const [themeChanged, setThemeChanged] = useState(false);
-
-  // initialize searchText as empty string
   const [searchText, setSearchText] = useState("");
-
-  // initialize the tooltip as not visible
   const [isTooltipVisible, setIsTooltipVisible] = useState(false);
 
-  // define array of objects to determine theme options
-  // for reasons unknown to me, the default theme has to have a rgb value
-  // it doesn't get converted like the other values do. this is a mystery i have yet to solve
+  /*
+   * default theme value must be an rgb value, not a hex value.
+   * for the other themes, the browser will convert hex to rgb when the color
+   * gets applied, but not for default. i am not sure why.
+   */
   const themeOptions = [
     {
       id: "default",
@@ -56,7 +48,6 @@ export default function App() {
     },
   ];
 
-  // functions to handle state for the tooltip
   function handleMouseEnter() {
     setIsTooltipVisible(true);
   }
@@ -64,18 +55,14 @@ export default function App() {
     setIsTooltipVisible(false);
   }
 
-  // gets called when user selects from theme dropdown menu
   function switchTheme(selectedValue) {
-    // if the selection is different from the current theme, flip themeChanged to true
     if (theme !== selectedValue) {
       setThemeChanged(true);
 
-      // if the user selected the default value, set theme, selectedThemeOption to default and store in browser
       if (selectedValue === "rgb(255, 255, 224)") {
         setTheme("default");
         setSelectedThemeOption("default");
         localStorage.setItem("theme", JSON.stringify("default"));
-        // if the user selected anything other than default, set theme, selectedThemeOption and store in browser
       } else {
         setTheme(selectedValue);
         setSelectedThemeOption(selectedValue);
@@ -84,28 +71,18 @@ export default function App() {
     }
   }
 
-  // effect that runs anytime theme, themeChanged are updated
   useEffect(() => {
-    // if the theme is default, don't do anything here
     if (theme === "default") {
       return;
-      // if the theme is anything other than default, fetch colors from API according to which theme is active
     } else {
       fetch(`https://www.thecolorapi.com/scheme?hex=${theme}&mode=analogic`)
         .then((response) => response.json())
         .then((data) => {
-          /*
-           * something i don't understand - i am pulling hex values here but
-           * later somehow they will be converted to rgb when they get
-           * inserted into the HTML structure - i need to figure out why/how
-           * */
+          // pull in hex values that will get converted to rgb in browser
           const hexValues = data.colors.map((color) => color.hex.value);
-          // set the background colors array to be the hex values we fetched
           setBackgroundColors(hexValues);
         })
         .catch((error) => {
-          /* if the API fails, throw an error, set all note background color to default,
-          set selected theme option to default, and alert the user that feature is broken */
           console.log("Error fetching hex values from API", error);
           setNotes((prevNotes) =>
             prevNotes.map((note) => ({
@@ -114,21 +91,19 @@ export default function App() {
             }))
           );
           setSelectedThemeOption("default");
-          alert("Oops! Looks like the theme-switcher is broken.");
+          alert(
+            "Sorry! Looks like the theme-switcher is broken. Use the default theme for now."
+          );
         });
     }
   }, [theme, themeChanged]);
 
-  // effect that runs anytime theme, themeChanged, or backgroundColors are updated
   useEffect(() => {
-    // if we have a theme change, time to map over the notes and update the colors to reflect the new theme
     if (themeChanged) {
       setNotes((prevNotes) =>
         prevNotes.map((note) => ({
           ...note,
           backgroundColor:
-            /* if the theme is default, they should all be the same color (must be rgb val not hex val)
-            if not default, they should be random from the array of hex values we fetched */
             theme === "default"
               ? "rgb(255, 255, 224)"
               : backgroundColors[
@@ -139,17 +114,10 @@ export default function App() {
     }
   }, [theme, themeChanged, backgroundColors]);
 
-  // effect that runs every time notes is updated; stores notes in local browser
   useEffect(() => {
     localStorage.setItem("notes", JSON.stringify(notes));
   }, [notes]);
 
-  /*
-   * add a new note - if the theme is default, the color will be default,
-   * if not it will be a random color from our background colors array.
-   * if the color is default, it has to be set as a rgb value, and not a hex value.
-   * i have no idea why this is happening. it shouldn't be
-   */
   function addNote() {
     const randomColor =
       backgroundColors[Math.floor(Math.random() * backgroundColors.length)];
@@ -164,12 +132,10 @@ export default function App() {
     setNotes((prevNotes) => [newNote, ...prevNotes]);
   }
 
-  // remove a note
   function removeNote(clickedId) {
     setNotes((oldNotes) => oldNotes.filter((note) => note.id !== clickedId));
   }
 
-  // update a note's title or body text
   function onType(changedNoteId, updatedKey, updatedValue) {
     setNotes((prevNotes) =>
       prevNotes.map((note) => {
@@ -186,7 +152,6 @@ export default function App() {
     );
   }
 
-  // search through the active notes
   function onSearch(text) {
     const newSearchText = text.toLowerCase();
 
